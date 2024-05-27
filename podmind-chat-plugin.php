2229687ffaedf1fb12c3c0f4fe4b917befd11735
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-05-27 10:26:45 -0300

    refactor plugin

--- podmind-chat-plugin.php
+++ podmind-chat-plugin.php
@@ -5,19 +5,16 @@
  * Version: 0.0.1
  */
 
-// To avoid direct plugin access
-if (!defined('ABSPATH')) {
-  exit;
-};
-
-function render_embedded_html_element()
+function render_embedded_html_header()
 {
-  $html = '<voxgig-podmind-ask apikey="123customAPIKey" debug="true"></voxgig-podmind-ask>';
-  return $html;
+  echo '<script src="https://podmind.voxgig.com/widget/voxgig-podmind-ask.js"></script>';
 }
 
-add_action('wp_head', function () {
-  echo '<script src="https://podmind.voxgig.com/widget/voxgig-podmind-ask.js"></script>';
-});
+function render_embedded_html_body()
+{
+  return '<voxgig-podmind-ask apikey="" debug="true"></voxgig-podmind-ask>';
+}
 
-add_action('the_content', 'render_embedded_html_element');
+add_action('wp_head', 'render_embedded_html_header');
+
+add_action('the_content', 'render_embedded_html_body');
